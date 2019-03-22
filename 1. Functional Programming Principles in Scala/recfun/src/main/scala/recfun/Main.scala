bdd08893cdedbd2ebfee9bb315123a7ca8e403bb
Igor F. Yago <igor.yago@gmail.com>
2019-03-22 00:14:56 +0000

    w1

--- 1. Functional Programming Principles in Scala/recfun/src/main/scala/recfun/Main.scala
+++ 1. Functional Programming Principles in Scala/recfun/src/main/scala/recfun/Main.scala
@@ -13,15 +13,38 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
-  
+    def pascal(c: Int, r: Int): Int = {
+      if (c <= 0 || c == r) 1
+      else pascal(c, r-1) + pascal(c-1, r-1)
+    }
+
   /**
    * Exercise 2
    */
-    def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def balanced(chars: List[Char], open: Int): Boolean = {
+      if (chars.isEmpty) open == 0
+      else
+      if (chars.head == '(') balanced(chars.tail,open+1)
+      else
+      if (chars.head == ')') open>0 && balanced(chars.tail,open-1)
+      else balanced(chars.tail,open)
+    }
+    balanced(chars,0)
+  }
   
   /**
    * Exercise 3
    */
-    def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+
+    def loop(money: Int, coins: List[Int]): Int = {
+      if (money < 0 || coins.isEmpty) 0
+      else if (money == 0) 1
+      else loop(money, coins.tail) + loop(money - coins.head, coins)
+    }
+
+    loop(money, coins)
   }
+}
+
